ce28a5b94cab689542d5cd530e98d7c8096cabaf
Eriuden <antoinecarrette@hotmail.fr>
2023-12-01 22:39:09 +0100

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,13 @@
 
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
   
 // Remplacer certaines valeurs par des icones plus tard
   return (
     <div>
-      <header className='p-2 flex justify-between'>
-        <a href='' className='flex items-center gap-1'>TerreBNB</a>
-        <div className='flex gap-2 border border-gray-300 rounded-full 
-        py-2 px-4 shadow-md shadow-gray-300'>
-          <div>N'importe où</div>
-          <div className='border-l border-gray-300'></div>
-          <div>N'importe quand</div>
-          <div className='border-l border-gray-300'></div>
-          <div>Avec n'importe qui</div>
-          <button className='bg-primary text-white p-2 rounded-full'>
-            Explorez
-          </button>
-        </div>
-        <div className='flex gap-2 border border-gray-300 rounded-full 
-        py-2 px-4'>+</div>
-        <div className='bg-gray-500 text-white rounded-full p-1 border border-gray-500 '>user</div>
-      </header>
+      <Header/>
     </div>
   )
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className='p-2 flex justify-between'>
+        <a href='' className='flex items-center gap-1'>TerreBNB</a>
+        <div className='flex gap-2 border border-gray-300 rounded-full 
+        py-2 px-4 shadow-md shadow-gray-300'>
+          <div>N'importe où</div>
+          <div className='border-l border-gray-300'></div>
+          <div>N'importe quand</div>
+          <div className='border-l border-gray-300'></div>
+          <div>Avec n'importe qui</div>
+          <button className='bg-primary text-white p-2 rounded-full'>
+            Explorez
+          </button>
+        </div>
+        <div className='flex gap-2 border border-gray-300 rounded-full 
+        py-2 px-4'>+</div>
+        <div className='bg-gray-500 text-white rounded-full p-1 border border-gray-500 '>
+          user
+        </div>
+      </header>
+  )
+}
