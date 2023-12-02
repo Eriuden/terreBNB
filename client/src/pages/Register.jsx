23713a53905b181fe3dbc6bbe7179324303464f7
Antoine Carrette <71089756+Eriuden@users.noreply.github.com>
2023-12-02 10:11:52 +0100

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,7 +6,7 @@ export const Register = () => {
          <div className='mb-16'>
             <h1 className='text-4xl text-center'>Inscription</h1>
             <form className='max-w-md mx-auto border'>
-            <input className='w-full border my-1 py-2 px-3 rounded-2xl' type="email" placeholder="Votre nom"/>
+                <input className='w-full border my-1 py-2 px-3 rounded-2xl' type="email" placeholder="Votre nom"/>
                 <input className='w-full border my-1 py-2 px-3 rounded-2xl' type="email" placeholder="Votre adresse mail"/>
                 <input className='w-full border my-1 py-2 px-3 rounded-2xl' type="password" placeholder="Votre mot de passe"/>
                 <button className=' bg-gray-300 w-full text-white rounded-2xl'>S'inscrire</button>
